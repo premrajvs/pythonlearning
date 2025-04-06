e356162dc7bf0d275992a26792ca02605f890219
premrajvs <premrajvs@gmail.com>
2025-04-05 19:00:59 -0500

    01 comments added

--- 01 variables and datatypes.py
+++ 01 variables and datatypes.py
@@ -2,3 +2,7 @@
 name = "John"
 age = 38
 print(f"Hello, my name is {name} and I am {age} years old.")
+# you may get a question of why use the variable f instead the curved bracket.
+# It is called f-string. This allows you to embed string expression (name and age variables)
+# within the string literals.
+# Try running the code without f and see what happens
