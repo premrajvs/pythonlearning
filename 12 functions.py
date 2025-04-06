ace25d03842a6a417e138256a233f80486554eb9
premrajvs <premrajvs@gmail.com>
2025-04-05 20:50:59 -0500

    functions with variables and return

--- 12 functions.py
+++ 12 functions.py
@@ -1,9 +1,7 @@
 # Let's say you want to do some task repeatedly. Instead of writing the same code over and over
 # you can defne it as a method and call the method to perform the action many times
-def add(x, y):
-    return x + y
+def sayhello():
+    print ("hello")
 
-result = add(3, 5)
-print(f"The sum is: {result}")
-result = add(1, 2)
-print(f"The sum is: {result}")
+sayhello()
+sayhello()
---
+++ 13 variables in functions.py
@@ -0,0 +1,6 @@
+# Scenario: A function that greets a person by name.
+def greet(name):
+    return f"Hello, {name}!"
+
+greeting_message = greet("Charlie")
+print(greeting_message)
