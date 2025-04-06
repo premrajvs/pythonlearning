3c877094859ee3b48c56300bd85c0debe935217f
premrajvs <premrajvs@gmail.com>
2025-04-06 10:50:00 -0500

    added details of why this concept is needed

--- 33 oops.py
+++ 33 oops.py
@@ -2,6 +2,10 @@
 '''
 In basic level concepts, we went through many variable types that can hold data of one type.
 Class is a data type that can hold a collection
+
+Why & Where: Fundamental for structuring complex applications, promoting code reusability and maintainability.
+Real World: Modeling real-world entities (users, products, orders), building frameworks and libraries, GUI development, game development.
+
 '''
 class Car:
     def __init__(self, make, model, color="black"):
