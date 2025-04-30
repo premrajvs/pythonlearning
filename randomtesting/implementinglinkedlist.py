ce0ce32a3c924e9b50edf2cde2642b8b55cd6c11
premrajvs <premrajvs@gmail.com>
2025-04-30 15:51:49 -0500

    linked list - added delete function

--- randomtesting/implementinglinkedlist.py
+++ randomtesting/implementinglinkedlist.py
@@ -19,12 +19,12 @@ class customLinkedList():
             print(f"Element: {element.value} NextElement: {element.nextelement}")
             return
         print(element.value)
-        print(element.nextelement)
+        #print(element.nextelement)
         while element is not None:
             element = element.nextelement
             if element is not None:
                 print(element.value)
-                print(element.nextelement)
+                #print(element.nextelement)
 
     def insetelement(self,*values):
         for item in values:
@@ -37,14 +37,50 @@ class customLinkedList():
                 self.element.nextelement = newelement
                 self.element = newelement
                 print(f"new current element value: {self.element.value}")
-            
+
+    def deleteelement(self,value):
+        todelete = elements(value)
+        self.element = self.head
+        while True:         
+            if self.head.value is None:
+                print("List is empty")
+                return
+            elif self.element is None:
+                print("value do not exist")
+                return
+            else:
+                if self.element.value == self.head.value and self.element.nextelement == self.head.nextelement:
+                    #this is the first element. So, it does not have any previous elements.
+                    #we only have to delete this element and move head to next element
+                    if self.element.value == todelete.value:
+                        self.head = self.element.nextelement
+                        self.element = None
+                        self.element = self.head
+                        return
+                    #this is the first element and it is not equal to the element to be deleted.
+                    #so, assigning this to a temp previous element
+                    else:
+                        self.previouslement = self.element
+                        self.element = self.element.nextelement
+                else:
+                    #check if current value is equal to element to be deleted in a loop
+                    # if yes, change previous element next element to this element
+                    if todelete.value == self.element.value:
+                        self.previouslement.nextelement = self.element.nextelement
+                        self.element.value = None
+                        self.element.nextelement = None
+                        break
+                    else:
+                        self.element = self.element.nextelement
 
 cl1 = customLinkedList()
 cl1.printcustomlinkedlist()
 cl1.insetelement(3,4,5)
-cl1.insetelement(6)
-cl1.printcustomlinkedlist()
+#cl1.insetelement(6)
+#cl1.printcustomlinkedlist()
 #cl1.addelement(1)
+cl1.deleteelement(7)
+cl1.printcustomlinkedlist()
 
 """
 This is another way of implementing but it users inheritance. 
