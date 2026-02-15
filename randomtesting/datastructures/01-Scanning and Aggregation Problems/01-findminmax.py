8e441a4b7c1322612e232bf8cdc921138d1387e9
premrajvs <premrajvs@gmail.com>
2026-02-14 23:23:11 -0600

    practice

--- randomtesting/datastructures/01-Scanning and Aggregation Problems/01-findminmax.py
+++ randomtesting/datastructures/01-Scanning and Aggregation Problems/01-findminmax.py
@@ -10,6 +10,9 @@
 
  In Java you can have multiple classes and functions and then declare void main function in on class and it automatically becomes the entry point. But python runs top down. So, it does not work that way. When you import the python class in another class in a big project, you dont have to worry about what gets called when because code logic takes care of that. But if you want to run a file directly, use __name__ == "__main__". Python knows to run this automatically.
  """
+from typing import List
+
+
 class findminmax:
     def checkalllinearly(self,arrayofnumber):
         if not isinstance(arrayofnumber,(list)):
@@ -36,6 +39,33 @@ class callingclass:
         data = [1,3,5,2,4,6]
         print(fi.checkalllinearly(data))
 
+class Solution:
+    def maxProfit_DoubleLoop(self, prices: List[int]) -> int:
+        if len(prices)<1:
+            return 0
+        profit = 0
+        for i in range(0,len(prices)):
+            for j in range(i+1,len(prices)):
+                if prices[j] - prices[i]>profit and prices[j]>prices[i]:
+                    profit = prices[j] - prices[i]
+        return profit
+    
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)<1:
+            return 0
+        profit = 0
+        minimumprice = prices[0]
+        maxprofit = 0
+        for i in range(0,len(prices)):
+
+            if prices[i]<minimumprice:
+                minimumprice = prices[i]
+            elif prices[i] - minimumprice > maxprofit:
+                maxprofit = prices[i] - minimumprice
+        return maxprofit
+
 if __name__ == "__main__":
     c1 = callingclass()
     c1.main(None)
+    s1 = Solution()
+    s1.maxprofile()
