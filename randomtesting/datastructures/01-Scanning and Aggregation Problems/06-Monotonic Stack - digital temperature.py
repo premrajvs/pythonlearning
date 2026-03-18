2ccda462845757673c35f8f0a65ee1ba110bd44e
premrajvs <premrajvs@gmail.com>
2026-03-17 22:31:50 -0500

    monotonic stack : practice 2

--- randomtesting/datastructures/01-Scanning and Aggregation Problems/06-Monotonic Stack - digital temperature.py
+++ randomtesting/datastructures/01-Scanning and Aggregation Problems/06-Monotonic Stack - digital temperature.py
@@ -14,3 +14,32 @@ Example 3:
 
 Input: temperatures = [30,60,90]
 Output: [1,1,0] """
+
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        tempstack = []
+        ipos = 0
+        posmap = {}
+        valmap = {}
+        for num in temperatures:
+            posmap[num] = ipos
+            valmap[num] = 0
+            while tempstack:
+                temp = tempstack.pop()
+                if num > temp:
+                    valmap[temp] = posmap[num]-posmap[temp]
+                else:
+                    tempstack.append(temp)
+                    break
+            tempstack.append(num)   
+            ipos = ipos + 1
+        
+        return [valmap[num] for num in temperatures]
+
+if __name__ == "__main__":
+    c1 = Solution()
+    c1.dailyTemperatures([73,74,75,71,69,72,76,73])
